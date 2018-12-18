3fa0db32a06e2591f0029344e67e475aaf89cfc1
PieterVanhoof <pieter.vanhoof2@gmail.com>
2018-12-18 11:05:47 +0100

    Initial commit

---
+++ src/main/java/be/ehb/eindopdrachtdierenwinkel/EindopdrachtdierenwinkelApplication.java
@@ -0,0 +1,14 @@
+package be.ehb.eindopdrachtdierenwinkel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtdierenwinkelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindopdrachtdierenwinkelApplication.class, args);
+    }
+
+}
+
