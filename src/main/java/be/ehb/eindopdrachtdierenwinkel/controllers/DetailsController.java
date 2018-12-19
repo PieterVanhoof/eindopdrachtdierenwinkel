76db772b38b26267464b8790f48ccfb56de19978
PieterVanhoof <pieter.vanhoof2@gmail.com>
2018-12-19 11:58:02 +0100

    Details controller added added broken details page

---
+++ src/main/java/be/ehb/eindopdrachtdierenwinkel/controllers/DetailsController.java
@@ -0,0 +1,28 @@
+package be.ehb.eindopdrachtdierenwinkel.controllers;
+
+import be.ehb.eindopdrachtdierenwinkel.model.Product;
+import be.ehb.eindopdrachtdierenwinkel.model.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DetailsController {
+    @Autowired
+    ProductRepository repo;
+
+    @RequestMapping(value = "/Details/{id}", method = RequestMethod.GET)
+    public String showDetailsForID(ModelMap map, @PathVariable(value = "id") int id){
+
+        Product p= repo.findById(id).get();
+        map.addAttribute("product", p);
+
+        return "Details";
+    }
+
+
+}
+
