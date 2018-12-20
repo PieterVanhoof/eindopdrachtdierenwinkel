223fe0af759b3fd09e5cbf0f7eb81c37124d70dc
PieterVanhoof <pieter.vanhoof2@gmail.com>
2018-12-20 14:06:52 +0100

    clean up cart

--- src/main/java/be/ehb/eindopdrachtdierenwinkel/controllers/WinkelwagenController.java
+++ src/main/java/be/ehb/eindopdrachtdierenwinkel/controllers/WinkelwagenController.java
@@ -1,5 +1,6 @@
 package be.ehb.eindopdrachtdierenwinkel.controllers;
 
+import be.ehb.eindopdrachtdierenwinkel.model.Product;
 import be.ehb.eindopdrachtdierenwinkel.model.ProductRepository;
 import be.ehb.eindopdrachtdierenwinkel.model.WinkelWagen;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,17 +8,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class WinkelwagenController {
-@Autowired
-private ProductRepository repo;
+    @Autowired
+    private ProductRepository repo;
 
-@RequestMapping(value = "/Winkelwagen)",method = RequestMethod.GET)
-public String getCart(ModelMap map){
+    @RequestMapping(value = "/Winkelwagen)", method = RequestMethod.GET)
+    public String getCart(ModelMap map) {
 
-    map.addAttribute("cart", WinkelWagen.getInstance().getCart());
+        map.addAttribute("cart", WinkelWagen.getInstance().getCart());
 
-    return "Details";
-}
+        return "Winkelwagen";
+    }
+
+    @RequestMapping(value = "Winkelwagen", method = RequestMethod.GET)
+    public String AddToCart(ModelMap map) {
+
+
+        return "Details";
+    }
 
 }
